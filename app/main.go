dd3c9351debed324348514d7635e67e5474f5547
reid <rscoggin@gmail.com>
2023-09-26 16:27:44 -0500

    linting

--- app/main.go
+++ app/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log/slog"
 	"net/http"
@@ -16,15 +16,15 @@ import (
 )
 
 var (
-	authkey = os.Getenv("AUTHKEY")
-	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	authkey     = os.Getenv("AUTHKEY")
+	logger      = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	versionData *structs.Version
 )
 
 func init() {
 	versionStr, err := db.Get()
 	if err != nil {
-		logger.Error(fmt.Sprintf("%v",err))
+		logger.Error(fmt.Sprintf("%v", err))
 	}
 	if versionStr == "" {
 		content, err := ioutil.ReadFile("default_vals.json")
@@ -55,82 +55,82 @@ func getConf(w http.ResponseWriter, r *http.Request) {
 }
 
 func updConf(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    channel := vars["channel"]
-    software := vars["software"]
-    key := vars["key"]
-    val := vars["value"]
-    var incomingValue interface{}
-    if val == "payload" {
-        bodyData := make(map[string]interface{})
-        err := json.NewDecoder(r.Body).Decode(&bodyData)
-        if err != nil {
-            http.Error(w, "Invalid payload", http.StatusBadRequest)
-            return
-        }
-        incomingValue = bodyData["value"]
-    } else {
-        incomingValue = val
-    }
-    dataMap := make(map[string]interface{})
-    jsonData, err := json.Marshal(versionData)
-    if err != nil {
-        http.Error(w, "Failed to marshal versionData", http.StatusInternalServerError)
-        return
-    }
-    err = json.Unmarshal(jsonData, &dataMap)
-    if err != nil {
-        http.Error(w, "Failed to unmarshal into dataMap", http.StatusInternalServerError)
-        return
-    }
-    groundseg, ok := dataMap["groundseg"].(map[string]interface{})
-    if !ok {
-        http.Error(w, "Invalid structure", http.StatusInternalServerError)
-        return
-    }
-    channelData, ok := groundseg[channel].(map[string]interface{})
-    if !ok {
-        http.Error(w, "Invalid channel", http.StatusBadRequest)
-        return
-    }
-    softwareData, ok := channelData[software].(map[string]interface{})
-    if !ok {
-        http.Error(w, "Invalid software", http.StatusBadRequest)
-        return
-    }
-    switch key {
-    case "minor", "major", "patch":
-        iVal, err := strconv.Atoi(fmt.Sprintf("%v", incomingValue))
-        if err != nil {
-            http.Error(w, "Invalid integer value", http.StatusBadRequest)
-            return
-        }
-        softwareData[key] = iVal
-    default:
-        softwareData[key] = incomingValue
-    }
-    jsonData, err = json.Marshal(dataMap)
-    if err != nil {
-        http.Error(w, "Failed to marshal dataMap", http.StatusInternalServerError)
-        return
-    }
-    err = json.Unmarshal(jsonData, &versionData)
-    if err != nil {
-        http.Error(w, "Failed to unmarshal into versionData", http.StatusInternalServerError)
-        return
-    }
-    blob, err := json.Marshal(versionData)
-    if err != nil {
-        http.Error(w, "Error encoding data", http.StatusInternalServerError)
-        return
-    }
-    if err = db.Update(string(blob)); err != nil {
-        http.Error(w, "Error saving to database", http.StatusInternalServerError)
-        return
-    }
-    logger.Info(fmt.Sprintf("Updated values: %+v", vars))
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("Updated successfully"))
+	vars := mux.Vars(r)
+	channel := vars["channel"]
+	software := vars["software"]
+	key := vars["key"]
+	val := vars["value"]
+	var incomingValue interface{}
+	if val == "payload" {
+		bodyData := make(map[string]interface{})
+		err := json.NewDecoder(r.Body).Decode(&bodyData)
+		if err != nil {
+			http.Error(w, "Invalid payload", http.StatusBadRequest)
+			return
+		}
+		incomingValue = bodyData["value"]
+	} else {
+		incomingValue = val
+	}
+	dataMap := make(map[string]interface{})
+	jsonData, err := json.Marshal(versionData)
+	if err != nil {
+		http.Error(w, "Failed to marshal versionData", http.StatusInternalServerError)
+		return
+	}
+	err = json.Unmarshal(jsonData, &dataMap)
+	if err != nil {
+		http.Error(w, "Failed to unmarshal into dataMap", http.StatusInternalServerError)
+		return
+	}
+	groundseg, ok := dataMap["groundseg"].(map[string]interface{})
+	if !ok {
+		http.Error(w, "Invalid structure", http.StatusInternalServerError)
+		return
+	}
+	channelData, ok := groundseg[channel].(map[string]interface{})
+	if !ok {
+		http.Error(w, "Invalid channel", http.StatusBadRequest)
+		return
+	}
+	softwareData, ok := channelData[software].(map[string]interface{})
+	if !ok {
+		http.Error(w, "Invalid software", http.StatusBadRequest)
+		return
+	}
+	switch key {
+	case "minor", "major", "patch":
+		iVal, err := strconv.Atoi(fmt.Sprintf("%v", incomingValue))
+		if err != nil {
+			http.Error(w, "Invalid integer value", http.StatusBadRequest)
+			return
+		}
+		softwareData[key] = iVal
+	default:
+		softwareData[key] = incomingValue
+	}
+	jsonData, err = json.Marshal(dataMap)
+	if err != nil {
+		http.Error(w, "Failed to marshal dataMap", http.StatusInternalServerError)
+		return
+	}
+	err = json.Unmarshal(jsonData, &versionData)
+	if err != nil {
+		http.Error(w, "Failed to unmarshal into versionData", http.StatusInternalServerError)
+		return
+	}
+	blob, err := json.Marshal(versionData)
+	if err != nil {
+		http.Error(w, "Error encoding data", http.StatusInternalServerError)
+		return
+	}
+	if err = db.Update(string(blob)); err != nil {
+		http.Error(w, "Error saving to database", http.StatusInternalServerError)
+		return
+	}
+	logger.Info(fmt.Sprintf("Updated values: %+v", vars))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Updated successfully"))
 }
 
 func authMiddleware(next http.Handler) http.Handler {
@@ -149,8 +149,8 @@ func authMiddleware(next http.Handler) http.Handler {
 }
 
 func capitalizeFirst(s string) string {
-    if s == "" {
-        return ""
-    }
-    return strings.ToUpper(string(s[0])) + s[1:]
+	if s == "" {
+		return ""
+	}
+	return strings.ToUpper(string(s[0])) + s[1:]
 }
